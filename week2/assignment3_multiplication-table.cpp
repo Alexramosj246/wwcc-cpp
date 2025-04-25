25fdfb7f4170e5fee41c5e3021416923f1a19c05
alexramos246@gmail.com <alexramos246@gmail.com>
2025-04-25 11:59:25 -0700

    adding assignment 3

---
+++ week2/assignment3_multiplication-table.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>  // For setw function
+using namespace std;
+
+int main() {
+    int start, end;
+
+    cout << "Multiplication Table Generator" << endl << endl;
+
+    cout << "Enter starting number: ";
+    cin >> start;
+
+    cout << "Enter ending number: ";
+    cin >> end;
+
+    // Validate input
+    if (start > end) {
+        cout << "Error: Starting number must be less than ending number." << endl;
+        return 1;
+    }
+
+    cout << endl;
+    
+    cout << setw(4) << " ";
+    for (int i = start; i <= end; ++i) {
+        cout << setw(4) << i;
+    }
+    cout << endl;
+
+    for (int i = start; i <= end; ++i) {
+        cout << setw(4) << i;
+        for (int j = start; j <= end; ++j) {
+            cout << setw(4) << i * j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
